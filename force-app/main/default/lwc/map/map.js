41225af15c5a6ae21837f8c4deed14d008f08a81
MielnickiB <mielnicki.bartek@gmail.com>
2024-05-28 08:04:39 +0200

    PI-009

--- force-app/main/default/lwc/map/map.js
+++ force-app/main/default/lwc/map/map.js
@@ -1,26 +1,33 @@
 import { LightningElement, api, wire } from 'lwc';
-import getShelters from '@salesforce/apex/mapController.getShelters'
+import getShelter from '@salesforce/apex/mapController.getShelter'
 
 export default class Map extends LightningElement {
     @api recordId;
     mapMarkers = [];
 
-    @wire(getShelters, {})
-    wiredAccount({ error, data }) {
+    @wire(getShelter, {shelterId : '$recordId'})
+    wiredMap({ error, data }) {
+        console.log(this.recordId);
         if(data) {
             console.log(data);
-            this.mapMarkers = [];
-            data.forEach((element) => {
-                console.log(element.City__c);
-                this.mapMarkers.push({location:{City: element.City__c, Country: element.Country__c, PostalCode: element.PostalCode__c, Street: element.Street__c}, title: element.Name})
-            });
-            console.log(this.mapMarkers);
+            this.mapMarkers = [
+                {
+                    location: {
+                        Street: data[0].Street__c,
+                        City: data[0].City__c,
+                        Country: data[0].Country__c,
+                        PostalCode: data[0].PostalCode__c
+                    },
+                    title: data[0].Name,
+                    description:
+                        data[0].City__c + ', ul.' + data[0].Street__c + '. Kod pocztowy: ' + data[0].PostalCode__c,
+                },
+            ];
         }
         else if(error) {
-            console.log(error);
+            console.error(error);
         }
     }
-    listView = 'visible';
     selectedMarkerValue = 'SF1';
     handleMarkerSelect(event) {
         this.selectedMarkerValue = event.target.selectedMarkerValue;
