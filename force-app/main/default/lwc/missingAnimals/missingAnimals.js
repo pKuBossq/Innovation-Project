4b763afcce7cc93551de75b6b2a9c8d71a883347
MielnickiB <mielnicki.bartek@gmail.com>
2024-06-16 00:20:51 +0200

    Wstępna końcowa wersja (możliwe poprawki)

--- force-app/main/default/lwc/missingAnimals/missingAnimals.js
+++ force-app/main/default/lwc/missingAnimals/missingAnimals.js
@@ -2,24 +2,20 @@ import { LightningElement, api, wire } from 'lwc';
 import getAllAnimalPhotos from '@salesforce/apex/MissingAnimals.getAllAnimalPhotos';
 
 export default class GetDayDelta extends LightningElement {
-  amount = 1;
-  mapaData = new Map();
-  mappedData = [];
+    amount = 1;
+    tempAmount = 1;
+    mapaData = new Map();
+    mappedData = [];
 
-  handleAmountChange(e) {
-    this.amount = parseInt(e.detail.value, 10);
-    console.log(this.amount);
-  }
+    handleInputChange(e) { this.tempAmount = parseInt(e.detail.value, 10); }
+    handleConfirmClick() { this.amount = this.tempAmount; }
 
-  @wire(getAllAnimalPhotos, { day: '$amount' })
-  wiredAnimals({ error, data }) {
-    if (data) {
-      const parsedData = JSON.parse(JSON.stringify(data));
-      this.mapaData = new Map(Object.entries(parsedData));
-      console.log(this.mapaData);
-      this.mappedData = Array.from(this.mapaData, ([key, value]) => ({ key, value }));
-    } else if (error) {
-      console.error('Error:', error);
+    @wire(getAllAnimalPhotos, { day: '$amount' })
+    wiredAnimals({ error, data }) {
+        if (data) {
+            const parsedData = JSON.parse(JSON.stringify(data));
+            this.mapaData = new Map(Object.entries(parsedData));
+            this.mappedData = Array.from(this.mapaData, ([key, value]) => ({ key, value }));
+        } else if (error) { console.error('Error:', error); }
     }
-  }
 }
