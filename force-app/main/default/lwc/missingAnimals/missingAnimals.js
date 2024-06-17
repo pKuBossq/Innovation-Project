e7c76eea046a4133d99759244a73550efd59bd86
MielnickiB <mielnicki.bartek@gmail.com>
2024-06-17 10:10:31 +0200

    Dodatkowe zmiany w lwc MissingAnimals

--- force-app/main/default/lwc/missingAnimals/missingAnimals.js
+++ force-app/main/default/lwc/missingAnimals/missingAnimals.js
@@ -4,18 +4,24 @@ import getAllAnimalPhotos from '@salesforce/apex/MissingAnimals.getAllAnimalPhot
 export default class GetDayDelta extends LightningElement {
     amount = 1;
     tempAmount = 1;
-    mapaData = new Map();
+    mapData = new Map();
     mappedData = [];
+    noAnimals = false;
 
     handleInputChange(e) { this.tempAmount = parseInt(e.detail.value, 10); }
+
     handleConfirmClick() { this.amount = this.tempAmount; }
 
     @wire(getAllAnimalPhotos, { day: '$amount' })
     wiredAnimals({ error, data }) {
         if (data) {
             const parsedData = JSON.parse(JSON.stringify(data));
-            this.mapaData = new Map(Object.entries(parsedData));
-            this.mappedData = Array.from(this.mapaData, ([key, value]) => ({ key, value }));
-        } else if (error) { console.error('Error:', error); }
+            this.mapData = new Map(Object.entries(parsedData));
+            this.mappedData = Array.from(this.mapData, ([key, value]) => ({ key, value }));
+            this.noAnimals = this.mappedData.length === 0;
+        } else if (error) {
+            console.error('Error:', error);
+            this.noAnimals = true;
+        }
     }
 }
